222163dc471092f3646e727ddcd97f9ecef89536
Nehul Dalwala <nehul283.rejoice@gmail.com>
2025-04-23 16:58:52 +0530

    refactor: Replace direct controller imports with containerController for improved organization

--- src/routes/container.routes.js
+++ src/routes/container.routes.js
@@ -1,32 +1,28 @@
 const express = require("express");
 const router = express.Router();
-const {
-  createContainer,
-  getAllContainers,
-  getContainerById,
-  updateContainer,
-  deleteContainer,
-} = require("../controllers/container.controller");
+
+
 const {
   createContainerSchema,
   updateContainerSchema,
 } = require("../validations/container.validation");
 const auth = require("../middlewares/auth");
 const validate = require("../middlewares/validate");
+const { containerController } = require("../controllers");
 
 // Create a new container   
-router.post("/", auth, validate(createContainerSchema), createContainer);
+router.post("/", auth, validate(createContainerSchema), containerController.createContainer);
 
 // Get all containers
-router.get("/", auth, getAllContainers);
+router.get("/", auth, containerController.getAllContainers);
 
 // Get a container by ID
-router.get("/:id", auth, getContainerById);
+router.get("/:id", auth, containerController.getContainerById);
 
 // Update a container
-router.put("/:id", auth, validate(updateContainerSchema), updateContainer);
+router.put("/:id", auth, validate(updateContainerSchema), containerController.updateContainer);
 
 // Delete a container
-router.delete("/:id", auth, deleteContainer);
+router.delete("/:id", auth, containerController.deleteContainer);
 
 module.exports = router;
