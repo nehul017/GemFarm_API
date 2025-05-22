1a00d3c34b5b176b8a7be16f36f78a54ad00a20d
Nehul Dalwala <nehul283.rejoice@gmail.com>
2025-05-22 16:17:37 +0530

    feat: Enhance container validation schemas with additional fields for size, planting, and harvest details

--- src/validations/container.validation.js
+++ src/validations/container.validation.js
@@ -14,7 +14,16 @@ const createContainerSchema = Joi.object({
     .try(Joi.string().isoDate(), Joi.date(), Joi.allow(null))
     .optional(),
   container_image: Joi.string().uri().optional().allow(null, ""),
+
+  container_size: Joi.string().optional().allow(null, ""),
+  num_plant_sites: Joi.number().integer().positive().optional().allow(null),
+  planting_date: Joi.alternatives()
+    .try(Joi.string().isoDate(), Joi.date(), Joi.allow(null))
+    .optional(),
+  time_to_first_harvest: Joi.string().optional().allow(null, ""),
+  harvest_frequency: Joi.string().optional().allow(null, ""),
 });
+
 
 const updateContainerSchema = Joi.object({
   autoGrowDeviceId: Joi.string().optional().allow(null, ""),
@@ -23,7 +32,15 @@ const updateContainerSchema = Joi.object({
   container_crop: Joi.string().optional().allow(null, ""),
   container_image: Joi.string().uri().optional().allow(null, ""),
   harvest_date: Joi.date().optional().allow(null, ""),
+  container_size: Joi.string().optional().allow(null, ""),
+  num_plant_sites: Joi.number().integer().positive().optional().allow(null),
+  planting_date: Joi.alternatives()
+    .try(Joi.string().isoDate(), Joi.date(), Joi.allow(null))
+    .optional(),
+  time_to_first_harvest: Joi.string().optional().allow(null, ""),
+  harvest_frequency: Joi.string().optional().allow(null, ""),
 }).min(1); // Ensure at least one field is provided
+
 
 module.exports = {
   createContainerSchema,
